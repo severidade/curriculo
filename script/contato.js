82ead3644251ca05dfbce042e59028ae4191e6a1
severidade <oliveira.severo@gmail.com>
2014-11-20 22:08:14 -0200

    commit-inicial

---
+++ script/contato.js
@@ -0,0 +1,9 @@
+ function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert(' ' + nome.value + ' sua mensagem foi encaminhada com sucesso. Obrigado.');
+    }
+
+} 
